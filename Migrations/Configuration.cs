15c5e506e53621ded4d34e1066c3677f47040ba1
Varsha Parekh <8116153+vparekh@users.noreply.github.com>
2018-05-10 16:46:35 -0500

    Add files via upload

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace SSPWebUI.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SSPWebUI.Areas.MVC.Models.EF.DataClassesDataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(SSPWebUI.Areas.MVC.Models.EF.DataClassesDataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
